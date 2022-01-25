846ff29362bcf3524676d35885f5ebb53d33a305
JaCrowe <dev@jeremycrowe.ca>
2022-01-25 18:31:50 -0400

    architecture is garbage but I'm now reading that file length properly for this case
     - Lots of generalization could be done!

--- src/pjaudiofile.cpp
+++ src/pjaudiofile.cpp
@@ -4,9 +4,13 @@
 #include <fstream>
 #include <SDL2/SDL.h>
 #include <fftw3.h>
-// #include <SDL2/SDL_opengl.h>
-// #include <SDL2/SDL_image.h>
+#define AUDIO_FRAME_BIT_DEPTH 8
 using namespace std;
+
+double getWavRunLength(SDL_AudioSpec wav_spec, Uint32 wav_length)
+{
+    return double(wav_length) / double(wav_spec.freq) / double(wav_spec.channels) / (double(SDL_AUDIO_BITSIZE(wav_spec.format)) / double(AUDIO_FRAME_BIT_DEPTH));
+}
 
 std::string PJAudioFile::getFileContents(ifstream &file)
 {
@@ -34,29 +38,25 @@ int PJAudioFile::loadfile(const char *filename)
     Uint8 *wav_buffer;
 
     /* Load the WAV */
-    if (SDL_LoadWAV(filename, &wav_spec, &wav_buffer, &wav_length) == NULL) {
+    if (SDL_LoadWAV(filename, &wav_spec, &wav_buffer, &wav_length) == NULL)
+    {
         fprintf(stderr, "Could not open test.wav: %s\n", SDL_GetError());
         return 1;
-    } else {
+    }
+    else
+    {
         /* Do stuff with the WAV data, and then... */
-        printf("Length of audio is: %d, %d, %d, %d, %d, %d, %d, %d, %f\n", 
-            wav_spec.freq, 
-            wav_spec.samples, 
-            wav_spec.freq,
-            wav_spec.format,
-            wav_spec.size,
-            wav_spec.channels,
-            wav_spec.padding,
-            wav_length,
-            double(wav_length)/double(wav_spec.freq)/60.
-        );
+        printf(
+            "Length of audio is: %f\n",
+            getWavRunLength(wav_spec, wav_length));
 
         SDL_FreeWAV(wav_buffer);
         return 0;
     }
 }
 
-PJAudioFile::PJAudioFile(const char *_file_path){
+PJAudioFile::PJAudioFile(const char *_file_path)
+{
     file_path = _file_path;
     loadfile(file_path);
 }
